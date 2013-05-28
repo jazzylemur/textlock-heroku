ee5d9502946a6d49072c7b5b8c834e00f8469d14
Deepa Prasad <jazzylemur@gmail.com>
2013-05-28 14:35:20 +0530

    mod settings.py. Added env var

--- settings.py
+++ settings.py
@@ -155,3 +155,6 @@ LOGGING = {
         },
     }
 }
+import os  
+ENVIRONMENT = os.environ.get('ENVIRONMENT', 'dev')  # dev, production, qa, etc  
+exec('from settings_%s import *' % ENVIRONMENT)  
