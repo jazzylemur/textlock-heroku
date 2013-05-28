03aa1f11d7d2e801ac5139c62aefc977e81e2a9d
Deepa Prasad <jazzylemur@gmail.com>
2013-05-28 13:33:01 +0530

    mod urls.py to match without /pools

--- pools/urls.py
+++ pools/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns,url
 from pools import views
 
 urlpatterns = patterns('',
-        url(r'^$',views.index,name='index')
+        url(r'^/pools$',views.index,name='index')
         url(r'^/two.html$',views.two,name='two'),
                        
                        )
