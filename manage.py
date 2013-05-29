48d6c1540cf928678e0403e80a3aaf6e206b4a42
jazzylemur <jazzylemur@gmail.com>
2013-05-29 20:12:59 +0530

    Update manage.py
    Manage.py->new

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+import imp
+
+APPS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'root', 'apps'))
+if APPS_DIR not in sys.path:
+    sys.path.insert(0, APPS_DIR)
+
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tester.settings")
-
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
