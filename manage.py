e1c89b41f154a374a656ca3523b365637a9604c9
jazzylemur <jazzylemur@gmail.com>
2013-05-29 21:01:09 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -9,6 +9,6 @@ if APPS_DIR not in sys.path:
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "textlock-heroku.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ancient-ravine-5775.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
